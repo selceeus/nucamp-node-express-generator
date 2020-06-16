a8d950955290411ccd88678d3efd81c100587d86
Matt Andrews <matt@carbonbasedcreative.com>
2020-06-15 20:58:26 -0600

    File uploads endpoint and file handling added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promtionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 //Establish Mongo Connection
 
@@ -58,6 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promtionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
